954843c2d0777d15247fbc9df94a58a996d58c43
Script Kid <translace@gmail.com>
2017-08-11 19:06:49 +0200

    update

--- source/conf.py
+++ source/conf.py
@@ -55,8 +55,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Bismuth'
-copyright = '2017, Jan Kucera'
-author = 'Jan Kucera'
+copyright = '2017, Bismuth Foundation'
+author = 'Bismuth Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -151,7 +151,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'Bismuth.tex', 'Bismuth Documentation',
-     'Jan Kucera', 'manual'),
+     'Bismuth Foundation', 'manual'),
 ]
 
 
